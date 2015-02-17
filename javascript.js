35b96f0d79ea423e4a499e1451fdd0e4d9aa3276
Danielv123 <tranen.bananen@gmail.com>
2015-02-17 22:19:16 +0100

    SORTING WORKING!!!! (mostly)

--- javascript.js
+++ javascript.js
@@ -2,13 +2,16 @@ window.onload = function() {
 document.getElementById("footerText").innerHTML =("<p>      &copy;  2014-" + new Date().getFullYear() + " Daniel Vestol. All rights reserved.</p>");
 };
 
+var drama = "drama"
+var action = "action"
+
 function removeContainer(matchClass) {
     var elems = document.getElementsByTagName('*'), i;
-	elems[i].style.display = ""
+	console.log(elems)
     for (i in elems) {
         if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
                 > -1) {
-            elems[i].style.display = "none";
+            elems[i].style.display="none";
         }
     }
 }
