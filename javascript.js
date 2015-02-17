735833a05e3528076235a295adbfcc30b1eb32bd
Daniel <tranen.bananen@gmail.com>
2015-02-17 14:05:01 -0800

    More prep

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
 window.onload = function() {
 document.getElementById("footerText").innerHTML =("<p>      &copy;  2014-" + new Date().getFullYear() + " Daniel Vestol. All rights reserved.</p>");
 };
+
+function removeContainer(matchClass) {
+    var elems = document.getElementsByTagName('*'), i;
+	elems[i].style.display = ""
+    for (i in elems) {
+        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
+                > -1) {
+            elems[i].style.display = "none";
+        }
+    }
+}
