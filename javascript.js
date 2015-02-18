a867f4cb12c16d9e8ae249c83690594dd47a010c
Daniel <tranen.bananen@gmail.com>
2015-02-18 11:04:59 -0800

    HUGE update, footer, adding reviews

--- javascript.js
+++ javascript.js
@@ -3,129 +3,129 @@ document.getElementById("footerText").innerHTML =("<p>      &copy;  2014-" + new
 };
 
 function poster1() {
-	window.location = poster1.html
+	window.location.href = "poster1.html";
 }
 
 function poster2() {
-	window.location = poster2.html
+	window.location.href = "poster2.html";
 }
 
 function poster3() {
-	window.location = poster3.html
+	window.location.href = "poster3.html";
 }
 
 function poster4() {
-	window.location = poster4.html
+	window.location.href = "poster4.html";
 }
 
 function poster5() {
-	window.location = poster5.html
+	window.location.href = "poster5.html";
 }
 
 function poster6() {
-	window.location = poster6.html
+	window.location.href = "poster6.html";
 }
 
 function poster7() {
-	window.location = poster7.html
+	window.location.href = "poster7.html";
 }
 
 function poster8() {
-	window.location = poster8.html
+	window.location.href = "poster8.html";
 }
 
 function poster9() {
-	window.location = poster9.html
+	window.location.href = "poster9.html";
 }
 
 function poster10() {
-	window.location = poster10.html
+	window.location.href = "poster10.html";
 }
 
 function poster11() {
-	window.location = poster11.html
+	window.location.href = "poster11.html";
 }
 
 function poster12() {
-	window.location = poster12.html
+	window.location.href = "poster12.html";
 }
 
 function poster13() {
-	window.location = poster13.html
+	window.location.href = "poster13.html";
 }
 
 function poster14() {
-	window.location = poster14.html
+	window.location.href = "poster14.html";
 }
 
 function poster15() {
-	window.location = poster15.html
+	window.location.href = "poster15.html";
 }
 
 function poster16() {
-	window.location = poster16.html
+	window.location.href = "poster16.html";
 }
 
 function poster17() {
-	window.location = poster17.html
+	window.location.href = "poster17.html";
 }
 
 function poster18() {
-	window.location = poster18.html
+	window.location.href = "poster18.html";
 }
 
 function poster19() {
-	window.location = poster19.html
+	window.location.href = "poster19.html";
 }
 
 function poster20() {
-	window.location = poster20.html
+	window.location.href = "poster20.html";
 }
 
 function poster21() {
-	window.location = poster21.html
+	window.location.href = "poster21.html";
 }
 
 function poster22() {
-	window.location = poster22.html
+	window.location.href = "poster22.html";
 }
 
 function poster23() {
-	window.location = poster23.html
+	window.location.href = "poster23.html";
 }
 
 function poster24() {
-	window.location = poster24.html
+	window.location.href = "poster24.html";
 }
 
 function poster25() {
-	window.location = poster25.html
+	window.location.href = "poster25.html";
 }
 
 function poster26() {
-	window.location = poster26.html
+	window.location.href = "poster26.html";
 }
 
 function poster27() {
-	window.location = poster27.html
+	window.location.href = "poster27.html";
 }
 
 function poster28() {
-	window.location = poster28.html
+	window.location.href = "poster28.html";
 }
 
 function poster29() {
-	window.location = poster29.html
+	window.location.href = "poster29.html";
 }
 
 function poster30() {
-	window.location = poster30.html
+	window.location.href = "poster30.html";
 }
 
 function poster31() {
-	window.location = poster31.html
+	window.location.href = "poster31.html";
 }
 
 function poster32() {
-	window.location = poster32.html
+	window.location.href = "poster32.html";
 }
