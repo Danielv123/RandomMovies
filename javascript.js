1825bb421d79d65b2a6cbc4d013e49552a0c43b9
Daniel <tranen.bananen@gmail.com>
2015-02-18 09:44:01 -0800

    Cropped images, JS links for movies, stuff

--- javascript.js
+++ javascript.js
@@ -2,16 +2,130 @@ window.onload = function() {
 document.getElementById("footerText").innerHTML =("<p>      &copy;  2014-" + new Date().getFullYear() + " Daniel Vestol. All rights reserved.</p>");
 };
 
-var drama = "drama"
-var action = "action"
+function poster1() {
+	window.location = poster1.html
+}
 
-function removeContainer(matchClass) {
-    var elems = document.getElementsByTagName('*'), i;
-	console.log(elems)
-    for (i in elems) {
-        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
-                > -1) {
-            elems[i].style.display="none";
-        }
-    }
+function poster2() {
+	window.location = poster2.html
+}
+
+function poster3() {
+	window.location = poster3.html
+}
+
+function poster4() {
+	window.location = poster4.html
+}
+
+function poster5() {
+	window.location = poster5.html
+}
+
+function poster6() {
+	window.location = poster6.html
+}
+
+function poster7() {
+	window.location = poster7.html
+}
+
+function poster8() {
+	window.location = poster8.html
+}
+
+function poster9() {
+	window.location = poster9.html
+}
+
+function poster10() {
+	window.location = poster10.html
+}
+
+function poster11() {
+	window.location = poster11.html
+}
+
+function poster12() {
+	window.location = poster12.html
+}
+
+function poster13() {
+	window.location = poster13.html
+}
+
+function poster14() {
+	window.location = poster14.html
+}
+
+function poster15() {
+	window.location = poster15.html
+}
+
+function poster16() {
+	window.location = poster16.html
+}
+
+function poster17() {
+	window.location = poster17.html
+}
+
+function poster18() {
+	window.location = poster18.html
+}
+
+function poster19() {
+	window.location = poster19.html
+}
+
+function poster20() {
+	window.location = poster20.html
+}
+
+function poster21() {
+	window.location = poster21.html
+}
+
+function poster22() {
+	window.location = poster22.html
+}
+
+function poster23() {
+	window.location = poster23.html
+}
+
+function poster24() {
+	window.location = poster24.html
+}
+
+function poster25() {
+	window.location = poster25.html
+}
+
+function poster26() {
+	window.location = poster26.html
+}
+
+function poster27() {
+	window.location = poster27.html
+}
+
+function poster28() {
+	window.location = poster28.html
+}
+
+function poster29() {
+	window.location = poster29.html
+}
+
+function poster30() {
+	window.location = poster30.html
+}
+
+function poster31() {
+	window.location = poster31.html
+}
+
+function poster32() {
+	window.location = poster32.html
 }
