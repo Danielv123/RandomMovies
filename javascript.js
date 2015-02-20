ea168af4a6c123df9b36e437b08b120cdc1b1e88
Daniel <tranen.bananen@gmail.com>
2015-02-19 16:01:45 -0800

    Thumbnails now also using PHP links

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ document.getElementById("footerText").innerHTML =("<p>      &copy;  2014-" + new
 //Aaaand it gives me some JS to show on github.
 
 function projectInfo() {
-	window.location.href = "ProjectInfo.html"
+	window.location.href = "ProjectInfo.php"
 }
 
 function kinocity() {
@@ -20,129 +20,129 @@ function kinocity() {
 }
 
 function poster1() {
-	window.location.href = "poster1.html";
+	window.location.href = "poster1.php";
 }
 
 function poster2() {
-	window.location.href = "poster2.html";
+	window.location.href = "poster2.php";
 }
 
 function poster3() {
-	window.location.href = "poster3.html";
+	window.location.href = "poster3.php";
 }
 
 function poster4() {
-	window.location.href = "poster4.html";
+	window.location.href = "poster4.php";
 }
 
 function poster5() {
-	window.location.href = "poster5.html";
+	window.location.href = "poster5.php";
 }
 
 function poster6() {
-	window.location.href = "poster6.html";
+	window.location.href = "poster6.php";
 }
 
 function poster7() {
-	window.location.href = "poster7.html";
+	window.location.href = "poster7.php";
 }
 
 function poster8() {
-	window.location.href = "poster8.html";
+	window.location.href = "poster8.php";
 }
 
 function poster9() {
-	window.location.href = "poster9.html";
+	window.location.href = "poster9.php";
 }
 
 function poster10() {
-	window.location.href = "poster10.html";
+	window.location.href = "poster10.php";
 }
 
 function poster11() {
-	window.location.href = "poster11.html";
+	window.location.href = "poster11.php";
 }
 
 function poster12() {
-	window.location.href = "poster12.html";
+	window.location.href = "poster12.php";
 }
 
 function poster13() {
-	window.location.href = "poster13.html";
+	window.location.href = "poster13.php";
 }
 
 function poster14() {
-	window.location.href = "poster14.html";
+	window.location.href = "poster14.php";
 }
 
 function poster15() {
-	window.location.href = "poster15.html";
+	window.location.href = "poster15.php";
 }
 
 function poster16() {
-	window.location.href = "poster16.html";
+	window.location.href = "poster16.php";
 }
 
 function poster17() {
-	window.location.href = "poster17.html";
+	window.location.href = "poster17.php";
 }
 
 function poster18() {
-	window.location.href = "poster18.html";
+	window.location.href = "poster18.php";
 }
 
 function poster19() {
-	window.location.href = "poster19.html";
+	window.location.href = "poster19.php";
 }
 
 function poster20() {
-	window.location.href = "poster20.html";
+	window.location.href = "poster20.php";
 }
 
 function poster21() {
-	window.location.href = "poster21.html";
+	window.location.href = "poster21.php";
 }
 
 function poster22() {
-	window.location.href = "poster22.html";
+	window.location.href = "poster22.php";
 }
 
 function poster23() {
-	window.location.href = "poster23.html";
+	window.location.href = "poster23.php";
 }
 
 function poster24() {
-	window.location.href = "poster24.html";
+	window.location.href = "poster24.php";
 }
 
 function poster25() {
-	window.location.href = "poster25.html";
+	window.location.href = "poster25.php";
 }
 
 function poster26() {
-	window.location.href = "poster26.html";
+	window.location.href = "poster26.php";
 }
 
 function poster27() {
-	window.location.href = "poster27.html";
+	window.location.href = "poster27.php";
 }
 
 function poster28() {
-	window.location.href = "poster28.html";
+	window.location.href = "poster28.php";
 }
 
 function poster29() {
-	window.location.href = "poster29.html";
+	window.location.href = "poster29.php";
 }
 
 function poster30() {
-	window.location.href = "poster30.html";
+	window.location.href = "poster30.php";
 }
 
 function poster31() {
-	window.location.href = "poster31.html";
+	window.location.href = "poster31.php";
 }
 
 function poster32() {
-	window.location.href = "poster32.html";
+	window.location.href = "poster32.php";
 }
