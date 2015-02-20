6b5382a395ddd7daa035e3c384fceacf418b9422
Daniel <tranen.bananen@gmail.com>
2015-02-19 16:40:37 -0800

    Fixed images not having alt="". For validation.

--- index.php
+++ index.php
@@ -11,41 +11,41 @@
 	<?php include('/header.html');?>
 	
 	<div id="previewBox">
-	<img class="importantImage" src="images/SharknadoPoster.jpg">
+	<img class="importantImage" src="images/SharknadoPoster.jpg" alt="SharknadoCover">
 	<h1>Sharknado</h1>
 	<h3>When a freak hurricane swamps Los Angeles, nature's deadliest killer rules sea, land, and air as thousands
 	of sharks terrorize the waterlogged populace. The main person has to save his family, and later, LA.</h3>
 	</div>
 	
 	<div id="movies">
-	<div class="movie"><img src="images/Poster1.jpg" onclick="poster1()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster3.jpg" onclick="poster3()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster4.jpg" onclick="poster4()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster5.jpg" onclick="poster5()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster6.jpg" onclick="poster6()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster7.jpg" onclick="poster7()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster8.jpg" onclick="poster8()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster9.jpg" onclick="poster9()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster10.jpg" onclick="poster10()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster11.jpg" onclick="poster11()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster12.jpg" onclick="poster12()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster13.jpg" onclick="poster13()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster14.jpg" onclick="poster14()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster15.jpg" onclick="poster15()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster16.jpg" onclick="poster16()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster17.jpg" onclick="poster17()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster18.jpg" onclick="poster18()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster19.jpg" onclick="poster19()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster20.jpg" onclick="poster20()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster21.jpg" onclick="poster21()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster22.jpg" onclick="poster22()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster23.jpg" onclick="poster23()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster25.jpg" onclick="poster25()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster26.jpg" onclick="poster26()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster27.jpg" onclick="poster27()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster28.jpg" onclick="poster28()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster29.jpg" onclick="poster29()" class="moviePoster"></div>
-	<div class="movie"><img src="images/Poster32.jpg" onclick="poster32()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster1.jpg" alt="coverpicture" onclick="poster1()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster3.jpg" alt="coverpicture" onclick="poster3()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster4.jpg" alt="coverpicture" onclick="poster4()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster5.jpg" alt="coverpicture" onclick="poster5()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster6.jpg" alt="coverpicture" onclick="poster6()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster7.jpg" alt="coverpicture" onclick="poster7()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster8.jpg" alt="coverpicture" onclick="poster8()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster9.jpg" alt="coverpicture" onclick="poster9()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster10.jpg" alt="coverpicture" onclick="poster10()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster11.jpg" alt="coverpicture" onclick="poster11()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster12.jpg" alt="coverpicture" onclick="poster12()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster13.jpg" alt="coverpicture" onclick="poster13()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster14.jpg" alt="coverpicture" onclick="poster14()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster15.jpg" alt="coverpicture" onclick="poster15()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster16.jpg" alt="coverpicture" onclick="poster16()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster17.jpg" alt="coverpicture" onclick="poster17()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster18.jpg" alt="coverpicture" onclick="poster18()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster19.jpg" alt="coverpicture" onclick="poster19()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster20.jpg" alt="coverpicture" onclick="poster20()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster21.jpg" alt="coverpicture" onclick="poster21()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster22.jpg" alt="coverpicture" onclick="poster22()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster23.jpg" alt="coverpicture" onclick="poster23()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster25.jpg" alt="coverpicture" onclick="poster25()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster26.jpg" alt="coverpicture" onclick="poster26()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster27.jpg" alt="coverpicture" onclick="poster27()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster28.jpg" alt="coverpicture" onclick="poster28()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster29.jpg" alt="coverpicture" onclick="poster29()" class="moviePoster"></div>
+	<div class="movie"><img src="images/Poster32.jpg" alt="coverpicture" onclick="poster32()" class="moviePoster"></div>
 	</div>
 	
 	<footer>
